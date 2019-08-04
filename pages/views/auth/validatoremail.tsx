6bba465afef26c7a565438e994f4b1ffd1af92a1
yiiu <yu.gintoki@gmail.com>
2019-08-04 22:45:11 +0800

    chore: update

--- pages/views/auth/validatoremail.tsx
+++ pages/views/auth/validatoremail.tsx
@@ -1,6 +1,7 @@
 import { rem } from 'polished';
 import React from 'react';
 import styled from 'styled-components';
+import { ICustomNextContext } from '@lib/common/interfaces/global';
 
 const Wrapper = styled.div`
   width: ${rem('700px')};
@@ -12,9 +13,16 @@ const Title = styled.div`
   font-size: ${_ => rem(_.theme.fontSizes[4])};
 `;
 
-export default () => (
+const ValidatorEmail = () => (
   <Wrapper>
     <Title>验证成功</Title>
     <div>validatoremail</div>
   </Wrapper>
 );
+
+(ValidatorEmail as any).getInitialProps = async (ctx: ICustomNextContext) => {
+  console.log(ctx.route.query);
+  return {};
+};
+
+export default ValidatorEmail;
