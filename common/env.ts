9d9cab6ddbe5d3a1d9a4833658bff890a12816b2
yiiu <yu.gintoki@gmail.com>
2019-09-16 11:49:52 +0800

    fix: env path bug

--- common/env.ts
+++ common/env.ts
@@ -3,5 +3,5 @@ import { resolve } from 'path';
 import { config } from 'dotenv';
 
 config({
-  path: process.env.NODE_ENV === 'production' ? resolve(__dirname, '../.env.production') : undefined,
+  path: process.env.NODE_ENV === 'production' ? resolve(process.cwd(), './.env.production') : undefined,
 });
