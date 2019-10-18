7fb3a3a40aba61f9188475a6dbc7c571d74fa822
yiiu <yu.gintoki@gmail.com>
2019-10-18 18:05:03 +0800

    chore: update  withData

--- lib/common/apollo/withData.ts
+++ lib/common/apollo/withData.ts
@@ -1,19 +1,70 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-boost';
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+// import { WebSocketLink } from 'apollo-link-ws';
+import { setContext } from 'apollo-link-context';
+import { ApolloLink } from 'apollo-link';
 import withData from 'next-with-apollo';
+import { onError } from 'apollo-link-error';
+
 import { server } from '../utils';
 
 function createClient({ headers, initialState }: any) {
-  return new ApolloClient({
-    uri: `${(server && process.env.NODE_ENV === 'production') ? 'http://127.0.0.1:3001' : process.env.URL}/graphql`,
-    cache: new InMemoryCache().restore(initialState || {}),
-    request: (operation) => {
-      operation.setContext({
-        headers: {
-          accept: 'application/json',
-          ...headers,
-        },
-      });
+  const URL = `${(server && process.env.NODE_ENV === 'production') ? 'http://127.0.0.1:3001' : process.env.URL}/graphql`;
+  const ssrMode = !(process as any).browser;
+
+  const httpLink = new HttpLink({
+    uri: URL,
+  });
+
+  // const wsLink = !ssrMode && new WebSocketLink({
+  //   uri: URL,
+  //   options: {
+  //     reconnect: true,
+  //     connectionParams: {
+  //       ...headers,
+  //     },
+  //   },
+  // });
+
+  const contextLink = setContext(
+    async () => ({
+      headers: {
+        accept: 'application/json',
+        ...headers,
+      },
+    }),
+  );
+
+  const errorLink = onError(
+    ({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(err => console.log(`[GraphQL error]: Message: ${err.message}`));
+      }
+      if (networkError) console.log(`[Network error]: ${networkError}`);
     },
+  );
+
+  const link = ApolloLink.from([
+    errorLink,
+    contextLink,
+    httpLink,
+    // wsLink as any,
+    // new RetryLink().split(
+    //   ({ query }) => {
+    //     const definition = getMainDefinition(query);
+    //     return (
+    //       definition.kind === 'OperationDefinition'
+    //       && definition.operation === 'subscription'
+    //     );
+    //   },
+    //   wsLink as any,
+    // ),
+  ]);
+  return new ApolloClient({
+    link,
+    ssrMode,
+    cache: new InMemoryCache().restore(initialState || {}),
   });
 }
 
