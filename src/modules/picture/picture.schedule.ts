060bd082cc37b9066d4d82e995fe40a3a8619b62
yiiu <yu.gintoki@gmail.com>
2019-12-12 18:05:15 +0800

    chore: Update hot picture

--- src/modules/picture/picture.schedule.ts
+++ src/modules/picture/picture.schedule.ts
@@ -3,18 +3,24 @@ import {
   Cron, NestSchedule,
 } from 'nest-schedule';
 import { PictureService } from '@server/modules/picture/picture.service';
+import { RedisService } from 'nestjs-redis';
+import dayjs from 'dayjs';
 
 @Injectable()
 export class PictureScheduleService extends NestSchedule {
   constructor(
     @Inject(forwardRef(() => PictureService))
     private readonly pictureService: PictureService,
+    private readonly redisService: RedisService,
   ) {
     super();
   }
 
-  @Cron('0 */3 * * *', { key: 'schedule-cron' })
-  public cron() {
-    // console.log('executing cron job');
+  @Cron('0 */3 * * *', { key: 'picture-hot' })
+  public async cron() {
+    const redisClient = this.redisService.getClient();
+    const data = await this.pictureService.getHotPictures();
+    await redisClient.zadd('picture_hot', ...data);
+    console.log(dayjs().format(), 'picture hot OK!!!!!!!!');
   }
 }
